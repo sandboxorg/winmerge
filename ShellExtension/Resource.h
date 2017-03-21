3a70c893b3fec6cad9b96ae812dfb13914fef458
GreyMerlin <GreyMerlin7@gmail.com>
2017-03-20 18:14:33 -0700

    Fix Memory Leak with Drop Targets
    Symptom:
    * There will be 4 memory leaks (each 80 bytes long) reported at
    	program termination for each File-View window that is opened.
    
    Discussion:
     * This is only noticeable with a _DEBUG compilation.
     * This will happen when either Text or Binary files are being
    	viewed/edited.
     * Each File-View window establishes four Drop Targets and registers
     	them with Windows.  Each also establishes an 'alternate' Drop
    	Target for use with various internal Drag-And-Drop operations.
     * When the edit windows are closed, the Drop Targets are properly
    	Revoke()'ed and deleted.
     * However: the 'alternate' Drop Targets are not deleted ... MemLeak!
    
    Implementation:
    * procedure OnDestroy() in file
    	Externals/crystaledit/editlib/ccrystaleditview.cpp now deletes
    	the 'alternate' Drop Target before deleting the master one.
    * Additionally, there is now an ASSERT() in procedure
    	SetAlternateDropTarget() guarding the assignment of the
    	'alternate' to make sure that it was still NULL

---
+++ ShellExtension/Resource.h
@@ -0,0 +1,33 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by ShellExtension.rc
+//
+#define IDS_PROJNAME                    100
+#define IDS_CONTEXT_MENU                101
+#define IDR_WINMERGESHELL               102
+#define IDS_CONTEXT_HELP                102
+#define IDS_CONTEXT_HELP_MANYITEMS      103
+#define IDS_COMPARE                     104
+#define IDS_COMPARE_ELLIPSIS            105
+#define IDS_COMPARE_TO                  106
+#define IDS_HELP_SAVETHIS               107
+#define IDS_HELP_COMPARESTORED          108
+#define IDS_HELP_COMPARESAVED           109
+#define IDS_RESELECT_FIRST              110
+#define IDB_WINMERGE                    202
+#define IDB_WINMERGEDIR                 203
+#define IDB_WINMERGE32                  204
+#define IDB_WINMERGEDIR32               205
+#define IDB_WINMERGE48                  206
+#define IDB_WINMERGEDIR48               207
+
+// Next default values for new objects
+//
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        208
+#define _APS_NEXT_COMMAND_VALUE         32768
+#define _APS_NEXT_CONTROL_VALUE         201
+#define _APS_NEXT_SYMED_VALUE           111
+#endif
+#endif
---
+++ Src/PropShell.cpp
@@ -0,0 +1,247 @@
+/** 
+ * @file  PropShell.cpp
+ *
+ * @brief Implementation file for Shell Options dialog.
+ *
+ */
+
+#include "stdafx.h"
+#include "PropShell.h"
+#include "RegKey.h"
+#include "OptionsMgr.h"
+#include "OptionsPanel.h"
+#include "Constants.h"
+#include "Environment.h"
+#include "paths.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+/// Flags for enabling and mode of extension
+#define CONTEXT_F_ENABLED 0x01
+#define CONTEXT_F_ADVANCED 0x02
+
+// registry values
+static LPCTSTR f_RegValueEnabled = _T("ContextMenuEnabled");
+static LPCTSTR f_RegValuePath = _T("Executable");
+
+static bool IsShellExtensionRegistered()
+{
+	HKEY hKey;
+	if (ERROR_SUCCESS == RegOpenKeyEx(HKEY_CLASSES_ROOT, _T("CLSID\\{4E716236-AA30-4C65-B225-D68BBA81E9C2}"), 0, KEY_QUERY_VALUE, &hKey)) {
+		RegCloseKey(hKey);
+		return true;
+	}
+	return false;
+}
+
+static bool RegisterShellExtension(bool unregister)
+{
+	TCHAR szSystem32[260] = { 0 };
+	TCHAR szSysWow64[260] = { 0 };
+	GetSystemDirectory(szSystem32, sizeof(szSystem32) / sizeof(szSystem32[0]));
+	GetSystemWow64Directory(szSysWow64, sizeof(szSysWow64) / sizeof(szSysWow64[0]));
+
+	String progpath = env::GetProgPath();
+	String regsvr32 = paths::ConcatPath(szSystem32, _T("regsvr32.exe"));
+	String args;
+	SHELLEXECUTEINFO sei = { sizeof(sei) };
+	sei.lpVerb = _T("runas");
+	if (szSysWow64[0])
+	{
+		args = (unregister ? _T("/s /u \"") : _T("/s \"")) + paths::ConcatPath(progpath, _T("ShellExtensionX64.dll")) + _T("\"");
+		sei.lpFile = regsvr32.c_str();
+		sei.lpParameters = args.c_str();
+		ShellExecuteEx(&sei);
+
+		regsvr32 = paths::ConcatPath(szSysWow64, _T("regsvr32.exe"));
+		args = (unregister ? _T("/s /u \"") : _T("/s \"")) + paths::ConcatPath(progpath, _T("ShellExtensionU.dll")) + _T("\"");
+		sei.lpFile = regsvr32.c_str();
+		sei.lpParameters = args.c_str();
+		return !!ShellExecuteEx(&sei);
+	}
+	else
+	{
+		args = (unregister ? _T("/s /u \"") : _T("/s \"")) + paths::ConcatPath(progpath, _T("ShellExtensionU.dll")) + _T("\"");
+		sei.lpFile = regsvr32.c_str();
+		sei.lpParameters = args.c_str();
+		return !!ShellExecuteEx(&sei);
+	}
+}
+
+PropShell::PropShell(COptionsMgr *optionsMgr) 
+: OptionsPanel(optionsMgr, PropShell::IDD)
+, m_bContextAdded(false)
+, m_bContextAdvanced(false)
+{
+}
+
+BOOL PropShell::OnInitDialog()
+{
+	OptionsPanel::OnInitDialog();
+
+#ifndef BCM_SETSHIELD
+#define BCM_SETSHIELD            (0x1600/*BCM_FIRST*/ + 0x000C)
+#endif
+
+	SendDlgItemMessage(IDC_REGISTER_SHELLEXTENSION, BCM_SETSHIELD, 0, TRUE);
+	SendDlgItemMessage(IDC_UNREGISTER_SHELLEXTENSION, BCM_SETSHIELD, 0, TRUE);
+
+	// Update shell extension checkboxes
+	UpdateButtons();
+	GetContextRegValues();
+	AdvancedContextMenuCheck();
+	UpdateData(FALSE);
+
+	SetTimer(0, 1000, NULL);
+
+	return TRUE;  // return TRUE  unless you set the focus to a control
+}
+
+void PropShell::DoDataExchange(CDataExchange* pDX)
+{
+	CPropertyPage::DoDataExchange(pDX);
+	//{{AFX_DATA_MAP(PropShell)
+	DDX_Check(pDX, IDC_EXPLORER_CONTEXT, m_bContextAdded);
+	DDX_Check(pDX, IDC_EXPLORER_ADVANCED, m_bContextAdvanced);
+	//}}AFX_DATA_MAP
+}
+
+BEGIN_MESSAGE_MAP(PropShell, CPropertyPage)
+	//{{AFX_MSG_MAP(PropShell)
+	ON_BN_CLICKED(IDC_EXPLORER_CONTEXT, OnAddToExplorer)
+	ON_BN_CLICKED(IDC_REGISTER_SHELLEXTENSION, OnRegisterShellExtension)
+	ON_BN_CLICKED(IDC_UNREGISTER_SHELLEXTENSION, OnUnregisterShellExtension)
+	ON_WM_TIMER()
+	//}}AFX_MSG_MAP
+END_MESSAGE_MAP()
+
+/** 
+ * @brief Reads options values from storage to UI.
+ */
+void PropShell::ReadOptions()
+{
+	GetContextRegValues();
+}
+
+/** 
+ * @brief Writes options values from UI to storage.
+ */
+void PropShell::WriteOptions()
+{
+	SaveMergePath(); // saves context menu settings as well
+}
+
+/// Get registry values for ShellExtension
+void PropShell::GetContextRegValues()
+{
+	CRegKeyEx reg;
+	LONG retVal = 0;
+	retVal = reg.Open(HKEY_CURRENT_USER, RegDir);
+	if (retVal != ERROR_SUCCESS)
+	{
+		String msg = strutils::format(_T("Failed to open registry key HKCU/%s:\n\t%d : %s"),
+			RegDir, retVal, GetSysError(retVal).c_str());
+		LogErrorString(msg);
+		return;
+	}
+
+	// Read bitmask for shell extension settings
+	DWORD dwContextEnabled = reg.ReadDword(f_RegValueEnabled, 0);
+
+	if (dwContextEnabled & CONTEXT_F_ENABLED)
+		m_bContextAdded = true;
+
+	if (dwContextEnabled & CONTEXT_F_ADVANCED)
+		m_bContextAdvanced = true;
+}
+
+/// Set registry values for ShellExtension
+void PropShell::OnAddToExplorer()
+{
+	AdvancedContextMenuCheck();
+	UpdateButtons();
+}
+
+/// Saves given path to registry for ShellExtension, and Context Menu settings
+void PropShell::SaveMergePath()
+{
+	TCHAR temp[MAX_PATH] = {0};
+	LONG retVal = 0;
+	GetModuleFileName(AfxGetInstanceHandle(), temp, MAX_PATH);
+
+	CRegKeyEx reg;
+	retVal = reg.Open(HKEY_CURRENT_USER, RegDir);
+	if (retVal != ERROR_SUCCESS)
+	{
+		String msg = strutils::format(_T("Failed to open registry key HKCU/%s:\n\t%d : %s"),
+			RegDir, retVal, GetSysError(retVal).c_str());
+		LogErrorString(msg);
+		return;
+	}
+
+	// Save path to WinMerge(U).exe
+	retVal = reg.WriteString(f_RegValuePath, temp);
+	if (retVal != ERROR_SUCCESS)
+	{
+		String msg = strutils::format(_T("Failed to set registry value %s:\n\t%d : %s"),
+			f_RegValuePath, retVal, GetSysError(retVal).c_str());
+		LogErrorString(msg);
+	}
+
+	// Determine bitmask for shell extension
+	DWORD dwContextEnabled = reg.ReadDword(f_RegValueEnabled, 0);
+	if (m_bContextAdded)
+		dwContextEnabled |= CONTEXT_F_ENABLED;
+	else
+		dwContextEnabled &= ~CONTEXT_F_ENABLED;
+
+	if (m_bContextAdvanced)
+		dwContextEnabled |= CONTEXT_F_ADVANCED;
+	else
+		dwContextEnabled &= ~CONTEXT_F_ADVANCED;
+
+	retVal = reg.WriteDword(f_RegValueEnabled, dwContextEnabled);
+	if (retVal != ERROR_SUCCESS)
+	{
+		String msg = strutils::format(_T("Failed to set registry value %s to %d:\n\t%d : %s"),
+			f_RegValueEnabled, dwContextEnabled, retVal, GetSysError(retVal).c_str());
+		LogErrorString(msg);
+	}
+}
+
+/// Enable/Disable "Advanced menu" checkbox.
+void PropShell::AdvancedContextMenuCheck()
+{
+	if (!IsDlgButtonChecked(IDC_EXPLORER_CONTEXT))
+	{
+		CheckDlgButton(IDC_EXPLORER_ADVANCED, FALSE);
+		m_bContextAdvanced = false;
+	}
+}
+
+void PropShell::UpdateButtons()
+{
+	bool registered = IsShellExtensionRegistered();
+	EnableDlgItem(IDC_EXPLORER_CONTEXT, registered);
+	EnableDlgItem(IDC_REGISTER_SHELLEXTENSION, !registered);
+	EnableDlgItem(IDC_UNREGISTER_SHELLEXTENSION, registered);
+	EnableDlgItem(IDC_EXPLORER_ADVANCED, 
+		registered && IsDlgButtonChecked(IDC_EXPLORER_CONTEXT));
+}
+
+void PropShell::OnRegisterShellExtension()
+{
+	RegisterShellExtension(false);
+}
+
+void PropShell::OnUnregisterShellExtension()
+{
+	RegisterShellExtension(true);
+}
+
+void PropShell::OnTimer(UINT_PTR nIDEvent)
+{
+	UpdateButtons();
+}
