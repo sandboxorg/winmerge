e0dc31cc35b0511bde577a0c908d6d7efe3f0997
GreyMerlin <GreyMerlin7@gmail.com>
2017-12-19 14:14:45 -0800

    Compile Merge with C++17 language rules.
     * Force the **Merge** project to be compiled with C++17 language
    semantics.
     * Ignore future (C++20?) deprecation warnings.
     * Use `unique_ptr<>` to replace `auto_ptr<>`
    
     * Force the **UnitTests** project to be compiled with the C++14
    language semantics.  To move this project forward to C++17 will require
    a Google Tests framework that is compatible with C++17.

---
+++ Src/OpenDoc.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "PathContext.h"
+#include "FileTransform.h"
+#include <array>
+
+class ProjectFile;
+
+// COpenDoc document
+
+class COpenDoc : public CDocument
+{
+	DECLARE_DYNCREATE(COpenDoc)
+
+public:
+	COpenDoc();
+	virtual ~COpenDoc();
+	void RefreshOptions();
+
+	std::array<DWORD, 3> m_dwFlags;
+	PathContext m_files;
+	bool	m_bRecurse;
+	String	m_strExt;
+	String	m_strUnpacker;
+	PackingInfo m_infoHandler;
+
+protected:
+	virtual BOOL OnNewDocument();
+
+	DECLARE_MESSAGE_MAP()
+};
---
+++ Version.h
@@ -0,0 +1,4 @@
+#define FILEVER        2,15,2,0
+#define PRODUCTVER     2,15,2,0
+#define STRFILEVER     "2.15.2.0"
+#define STRPRODUCTVER  "2.15.2.0"
